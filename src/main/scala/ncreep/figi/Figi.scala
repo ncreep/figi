599247856cada06208b6e8b56c394d7e98768fe5
Daniel Beskin <daniel.beskin@gmail.com>
2013-06-27 21:17:12 +0300

    Adding support for default arguments.

--- src/main/scala/ncreep/figi/Figi.scala
+++ src/main/scala/ncreep/figi/Figi.scala
@@ -15,12 +15,6 @@ trait Conf {
   def getWithDefault[A](conf: ConfNames, default: A)(implicit conv: ConfConverter[A]): A
 }
 
-//TODO testing only
-object Conf extends Conf {
-  def get[A](conf: ConfNames)(implicit conv: ConfConverter[A]): A = { println(conf); null.asInstanceOf[A] }
-  def getWithDefault[A](conf: ConfNames, default: A)(implicit conv: ConfConverter[A]): A = { println(conf + " - " + default); null.asInstanceOf[A] }
-}
-
 object Figi {
   //TODO macros do not support default args
   def makeConf[A](
@@ -30,13 +24,13 @@ object Figi {
   def makeConfImpl[A](con: Context)(
     conf: con.Expr[Conf],
     prefix: con.Expr[ConfNames])(implicit tag: con.WeakTypeTag[A]): con.Expr[A] = {
-    import con.universe._
 
     class Helper extends QuasiquoteCompat {
       val c: con.type = con
       import c.universe._
+      
       val tpe = tag.tpe
-
+      
       val impls: Iterable[Tree] = for {
         mem <- tpe.members
         if mem.isMethod
@@ -47,29 +41,47 @@ object Figi {
         termName = newTermName(name)
         t = meth.returnType.asInstanceOf[Type]
       } yield {
-    	  val getter = q"$conf.get[$t]($prefix :+ $name)"
-        //TODO is there a way to avoid dupilcation here?
+    	  val confName = q"$prefix :+ $name"
+    	  val getter = q"$conf.get[$t]($confName)"
         if (meth.isStable) { // val
           q"val $termName = $getter"
         } else { // def
-          q"def $termName = $getter"
-        }
+          def nullaryDef = q"def $termName = $getter"
+          mem.typeSignature match {
+            case NullaryMethodType(_) => nullaryDef
+            case MethodType(Nil, _) => nullaryDef
+            case MethodType(arg :: Nil, _) => {
+              val argType = arg.typeSignature
+              val argName = newTermName("arg")
+              if (argType weak_<:< t) q"def $termName($argName: ${arg.typeSignature}) = $conf.getWithDefault[$t]($confName, $argName)"
+              else c.abort(c.enclosingPosition, s"Type mismatch in default configuration argument for method $name, $argType does not (weakly) conform to $t")
+            }
+            case _ => c.abort(c.enclosingPosition, s"Too many arguments in method ${name}")
+          }
+        } 
       }
 
       val typeName = newTypeName(tpe.typeSymbol.name.encoded)
       val impl = q"new $typeName {..$impls}"
-
+      println(impl)//RM
       val res = c.Expr[A](impl)
     }
 
     (new Helper).res
   }
-  
+
+  //TODO testing only
+  object Conf extends Conf {
+    def get[A](conf: ConfNames)(implicit conv: ConfConverter[A]): A = { println(conf); null.asInstanceOf[A] }
+    def getWithDefault[A](conf: ConfNames, default: A)(implicit conv: ConfConverter[A]): A = { println(conf + " - " + default); null.asInstanceOf[A] }
+  }
+
   implicit object Conv extends ConfConverter[Nothing] { def apply(conf: Try[ConfValue]) = ??? }
   trait Foo {
     def a(): Int
     val b: Double
     def c: List[Int]
+    def d(c:Int): Double
   }
 
 }
