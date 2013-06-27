2f12fd1af96cf65bb34b0b5b2f036e73ac5b67fc
Daniel Beskin <daniel.beskin@gmail.com>
2013-06-28 00:32:05 +0300

    doc fix

--- macros/src/main/scala/ncreep/figi/package.scala
+++ macros/src/main/scala/ncreep/figi/package.scala
@@ -7,15 +7,21 @@ package object figi {
   type ConfNames = Vector[String]
   type ConfValue = String
 
+  /** Converter between configuration names and configuration values. */
   trait ConfConverter[+A] {
     def apply(conf: Try[ConfValue]): A
   }
 
+  /** And abstraction for a configuration data type. */
   trait Conf {
+    /** @return The value that corresponds to the give configuration key. */
     def get[A](conf: ConfNames)(implicit conv: ConfConverter[A]): A
+    /** 
+     *  @return The value that corresponds to the give configuration key, or the default in case the key is missing. 
+     */
     def getWithDefault[A](conf: ConfNames, default: A)(implicit conv: ConfConverter[A]): A
   }
 
-  /** Marker trait for configuration types that should chain `Figi.makeConf` invocations. */
+  /** A marker trait for configuration types that should chain `Figi.makeConf` invocations. */
   trait ConfChainer
 }
